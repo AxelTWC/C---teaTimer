72d6431ad4e215dbc5118d356905d5c88d9d8b1e
AxelTWC <30543699+AxelTWC@users.noreply.github.com>
2021-11-19 22:02:12 -0500

    Add files via upload

---
+++ teaTimer/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace teaTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tea1 = new System.Windows.Forms.PictureBox();
+            this.tea2 = new System.Windows.Forms.PictureBox();
+            this.tea3 = new System.Windows.Forms.PictureBox();
+            this.teaPot = new System.Windows.Forms.PictureBox();
+            this.intro = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.teaLabel1 = new System.Windows.Forms.Label();
+            this.teaLabel2 = new System.Windows.Forms.Label();
+            this.teaLabel3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.temperature = new System.Windows.Forms.Label();
+            this.start1 = new System.Windows.Forms.Button();
+            this.start2 = new System.Windows.Forms.Button();
+            this.start3 = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.sandTimer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.tea1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tea2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tea3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teaPot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sandTimer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tea1
+            // 
+            this.tea1.BackColor = System.Drawing.Color.Transparent;
+            this.tea1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tea1.BackgroundImage")));
+            this.tea1.Location = new System.Drawing.Point(12, 557);
+            this.tea1.Name = "tea1";
+            this.tea1.Size = new System.Drawing.Size(201, 160);
+            this.tea1.TabIndex = 0;
+            this.tea1.TabStop = false;
+            this.tea1.Click += new System.EventHandler(this.tea1_Click);
+            // 
+            // tea2
+            // 
+            this.tea2.BackColor = System.Drawing.Color.Transparent;
+            this.tea2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tea2.BackgroundImage")));
+            this.tea2.Location = new System.Drawing.Point(279, 444);
+            this.tea2.Name = "tea2";
+            this.tea2.Size = new System.Drawing.Size(201, 160);
+            this.tea2.TabIndex = 1;
+            this.tea2.TabStop = false;
+            this.tea2.Click += new System.EventHandler(this.tea2_Click);
+            // 
+            // tea3
+            // 
+            this.tea3.BackColor = System.Drawing.Color.Transparent;
+            this.tea3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tea3.BackgroundImage")));
+            this.tea3.Location = new System.Drawing.Point(539, 557);
+            this.tea3.Name = "tea3";
+            this.tea3.Size = new System.Drawing.Size(201, 160);
+            this.tea3.TabIndex = 2;
+            this.tea3.TabStop = false;
+            this.tea3.Click += new System.EventHandler(this.tea3_Click);
+            // 
+            // teaPot
+            // 
+            this.teaPot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.teaPot.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("teaPot.BackgroundImage")));
+            this.teaPot.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.teaPot.Location = new System.Drawing.Point(279, 56);
+            this.teaPot.Name = "teaPot";
+            this.teaPot.Size = new System.Drawing.Size(200, 200);
+            this.teaPot.TabIndex = 3;
+            this.teaPot.TabStop = false;
+            // 
+            // intro
+            // 
+            this.intro.AutoSize = true;
+            this.intro.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.intro.Location = new System.Drawing.Point(218, 283);
+            this.intro.Name = "intro";
+            this.intro.Size = new System.Drawing.Size(330, 29);
+            this.intro.TabIndex = 4;
+            this.intro.Text = "Click the tea you want to brew";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(316, 9);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(126, 29);
+            this.title.TabIndex = 5;
+            this.title.Tag = "";
+            this.title.Text = "Tea Timer";
+            // 
+            // teaLabel1
+            // 
+            this.teaLabel1.AutoSize = true;
+            this.teaLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teaLabel1.Location = new System.Drawing.Point(58, 557);
+            this.teaLabel1.Name = "teaLabel1";
+            this.teaLabel1.Size = new System.Drawing.Size(121, 29);
+            this.teaLabel1.TabIndex = 6;
+            this.teaLabel1.Text = "Black Tea";
+            // 
+            // teaLabel2
+            // 
+            this.teaLabel2.AutoSize = true;
+            this.teaLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teaLabel2.Location = new System.Drawing.Point(316, 444);
+            this.teaLabel2.Name = "teaLabel2";
+            this.teaLabel2.Size = new System.Drawing.Size(129, 29);
+            this.teaLabel2.TabIndex = 7;
+            this.teaLabel2.Text = "Green Tea";
+            // 
+            // teaLabel3
+            // 
+            this.teaLabel3.AutoSize = true;
+            this.teaLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.teaLabel3.Location = new System.Drawing.Point(576, 557);
+            this.teaLabel3.Name = "teaLabel3";
+            this.teaLabel3.Size = new System.Drawing.Size(123, 29);
+            this.teaLabel3.TabIndex = 8;
+            this.teaLabel3.Text = "White Tea";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer
+            // 
+            this.timer.AutoSize = true;
+            this.timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timer.Location = new System.Drawing.Point(204, 336);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(0, 29);
+            this.timer.TabIndex = 9;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(307, 283);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(0, 29);
+            this.timeLabel.TabIndex = 10;
+            this.timeLabel.Visible = false;
+            // 
+            // temperature
+            // 
+            this.temperature.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.temperature.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.temperature.Location = new System.Drawing.Point(516, 586);
+            this.temperature.Name = "temperature";
+            this.temperature.Size = new System.Drawing.Size(224, 120);
+            this.temperature.TabIndex = 11;
+            this.temperature.Visible = false;
+            // 
+            // start1
+            // 
+            this.start1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start1.Location = new System.Drawing.Point(279, 342);
+            this.start1.Name = "start1";
+            this.start1.Size = new System.Drawing.Size(75, 23);
+            this.start1.TabIndex = 12;
+            this.start1.Text = "Start";
+            this.start1.UseVisualStyleBackColor = true;
+            this.start1.Click += new System.EventHandler(this.start1_Click);
+            // 
+            // start2
+            // 
+            this.start2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start2.Location = new System.Drawing.Point(279, 341);
+            this.start2.Name = "start2";
+            this.start2.Size = new System.Drawing.Size(75, 23);
+            this.start2.TabIndex = 13;
+            this.start2.Text = "Start";
+            this.start2.UseVisualStyleBackColor = true;
+            this.start2.Click += new System.EventHandler(this.start2_Click);
+            // 
+            // start3
+            // 
+            this.start3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start3.Location = new System.Drawing.Point(279, 342);
+            this.start3.Name = "start3";
+            this.start3.Size = new System.Drawing.Size(75, 23);
+            this.start3.TabIndex = 14;
+            this.start3.Text = "Start";
+            this.start3.UseVisualStyleBackColor = true;
+            this.start3.Click += new System.EventHandler(this.start3_Click_1);
+            // 
+            // reset
+            // 
+            this.reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.reset.Location = new System.Drawing.Point(405, 341);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 15;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // sandTimer
+            // 
+            this.sandTimer.BackColor = System.Drawing.Color.Transparent;
+            this.sandTimer.Image = ((System.Drawing.Image)(resources.GetObject("sandTimer.Image")));
+            this.sandTimer.Location = new System.Drawing.Point(259, 444);
+            this.sandTimer.Name = "sandTimer";
+            this.sandTimer.Size = new System.Drawing.Size(220, 220);
+            this.sandTimer.TabIndex = 16;
+            this.sandTimer.TabStop = false;
+            this.sandTimer.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(752, 729);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.start3);
+            this.Controls.Add(this.start2);
+            this.Controls.Add(this.start1);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.teaLabel3);
+            this.Controls.Add(this.teaLabel2);
+            this.Controls.Add(this.teaLabel1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.intro);
+            this.Controls.Add(this.teaPot);
+            this.Controls.Add(this.tea3);
+            this.Controls.Add(this.tea2);
+            this.Controls.Add(this.tea1);
+            this.Controls.Add(this.sandTimer);
+            this.Controls.Add(this.temperature);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tea1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tea2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tea3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teaPot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sandTimer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox tea1;
+        private System.Windows.Forms.PictureBox tea2;
+        private System.Windows.Forms.PictureBox tea3;
+        private System.Windows.Forms.PictureBox teaPot;
+        private System.Windows.Forms.Label intro;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label teaLabel1;
+        private System.Windows.Forms.Label teaLabel2;
+        private System.Windows.Forms.Label teaLabel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label temperature;
+        private System.Windows.Forms.Button start1;
+        private System.Windows.Forms.Button start2;
+        private System.Windows.Forms.Button start3;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.PictureBox sandTimer;
+    }
+}
+
---
+++ teaTimer/Form1.cs
@@ -0,0 +1,195 @@
+﻿﻿/* teaTimerProgram 
+   Name: Axel Tang
+   Date: February 28
+   Teacher: Mrs.Schilstra
+   Purpose: Creating a Tea Timer Program
+*/
+
+ using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace teaTimer
+{
+
+    public partial class Form1 : Form
+    {
+        int timeLeft; //Set a variable for time left 
+        public Form1()
+        {
+            InitializeComponent();
+            start2.Visible = false; 
+            start3.Visible = false;
+            start1.Visible = false;
+            reset.Visible = false;
+        }
+        private void tea1_Click(object sender, EventArgs e) //A method for Black tea's icon 
+        {
+            temperature.Visible = true; //Shows temperature's text box
+            temperature.Text = "Optimal Temperature: 85°C-100°C"; //Temperature's text box set
+            intro.Text = "Click start to start timer\nClick reset to reset tea"; //set introduction's text
+            intro.Location = new Point(254, 283); //shift the introduction's location to the middle of the form
+            start1.Visible = true; //Shows the start and Reset button
+            reset.Visible = true;
+            tea2.Visible = false; //Make everything else invisible 
+            tea3.Visible = false;
+            teaLabel2.Visible = false;
+            teaLabel3.Visible = false;
+        }
+
+        private void tea2_Click(object sender, EventArgs e) //A method for Green tea's icon 
+        {
+            temperature.Visible = true;
+            temperature.Text = "Optimal Temperature: 70°C-85°C";
+            intro.Text = "Click start to start timer\nClick reset to reset tea";
+            intro.Location = new Point(254, 283);
+            start2.Visible = true;
+            reset.Visible = true;
+            tea1.Visible = false;
+            tea3.Visible = false;
+            teaLabel1.Visible = false;
+            teaLabel3.Visible = false;
+            tea2.Location = new Point(12, 557); //Set icon's location to Black tea's position 
+            teaLabel2.Location = new Point(58, 557);
+        }
+
+        private void tea3_Click(object sender, EventArgs e) //A method for White tea's icon 
+        {
+            temperature.Visible = true;
+            temperature.Text = "Optimal Temperature: 70°C-80°C";
+            intro.Text = "Click start to start timer\nClick reset to reset tea";
+            intro.Location = new Point(254, 283);
+            start3.Visible = true;
+            reset.Visible = true;
+            tea1.Visible = false;
+            tea2.Visible = false;
+            teaLabel1.Visible = false;
+            teaLabel2.Visible = false;
+            tea3.Location = new Point(12, 557);
+            teaLabel3.Location = new Point(58, 557);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //A method for timer
+        {
+            if (timeLeft > 0)
+            {
+                timeLabel.Visible = true;
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = (timeLeft + " seconds");
+            }
+            else
+            {
+                timer1.Stop();
+                System.Media.SystemSounds.Hand.Play(); //Plays a finish sound
+                MessageBox.Show("You Tea is Ready!", "Finished"); //Shows MessageBox once timer is up
+                tea1.Visible = true;
+                tea2.Visible = true;
+                tea3.Visible = true;
+                teaLabel1.Visible = true;
+                teaLabel2.Visible = true;
+                teaLabel3.Visible = true;
+                temperature.Visible = false;
+                timeLabel.Visible = false;
+                reset.Visible = false;
+                sandTimer.Visible = false;
+                intro.Visible = true;
+                reset.Visible = false;
+                start1.Visible = false;
+                start2.Visible = false;
+                start3.Visible = false;
+                tea2.Location = new Point(279, 444);
+                tea3.Location = new Point(539, 557);
+                teaLabel2.Location = new Point(316, 444);
+                teaLabel3.Location = new Point(576, 557);
+                sandTimer.Visible = false;
+                intro.Text = "Click the tea you want to brew"; //Set the intro back to the original text 
+                intro.Location = new Point(218, 283); //Goes back to original place 
+            }
+        }
+
+        private void start1_Click(object sender, EventArgs e) //When start button for Black Tea is clicked
+        {
+            click1(); 
+        }
+        public void click1() 
+        {
+            timeLabel.Visible = true; //Shows timeLabel
+            timeLabel.Text = ("240 seconds"); 
+            intro.Location = new Point(325, 617); //Set new location for intro label
+            timeLeft = 240; //4 Mintues , Set time to 240 seconds
+            timer1.Start(); //Starts the timer 
+            intro.Text = "Brewing";
+            start1.Visible = false; 
+            reset.Visible = false;
+            sandTimer.Visible = true;
+        }
+
+        private void start2_Click(object sender, EventArgs e) //When start button for Green Tea is clicked
+        {
+            click2();
+        }
+        public void click2()
+        {
+            timeLabel.Visible = true; //Shows timeLabel
+            timeLabel.Text = ("180 seconds");
+            intro.Location = new Point(325, 617); //Set new location for intro label
+            timeLeft = 180; //3 Mintues, Set time to 180 seconds
+            timer1.Start(); //Starts the timer 
+            intro.Text = "Brewing";
+            start2.Visible = false;
+            reset.Visible = false;
+            sandTimer.Visible = true;
+        }
+
+        private void start3_Click_1(object sender, EventArgs e) //When start button for White Tea is clicked
+        {
+            click3();
+        }
+        public void click3()
+        {
+            timeLabel.Visible = true; //Shows timeLabel
+            timeLabel.Text = ("120 seconds");
+            intro.Location = new Point(325, 617); //Set new location for intro label
+            timeLeft = 120; //2 Mintues , Set time to 120 seconds
+            timer1.Start();//Starts the timer
+            intro.Text = "Brewing";
+            start3.Visible = false;
+            reset.Visible = false;
+            sandTimer.Visible = true;
+        }
+
+        private void reset_Click(object sender, EventArgs e) //goto start does not apply here because its not a loop, therefore I manually disable and hide everything
+        {
+            resetChoice();
+        }
+        public void resetChoice()
+        {
+            temperature.Visible = false;
+            tea1.Visible = true;
+            tea2.Visible = true;
+            tea3.Visible = true;
+            teaLabel1.Visible = true;
+            teaLabel2.Visible = true;
+            teaLabel3.Visible = true;
+            intro.Visible = true;
+            reset.Visible = false;
+            start1.Visible = false;
+            start2.Visible = false;
+            start3.Visible = false;
+            tea2.Location = new Point(279, 444);
+            tea3.Location = new Point(539, 557);
+            teaLabel2.Location = new Point(316, 444);
+            teaLabel3.Location = new Point(576, 557);
+            sandTimer.Visible = false;
+
+
+        }
+    }
+}
